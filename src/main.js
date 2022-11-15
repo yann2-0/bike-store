05a05eaf6480ab2198bc99f671352454c5693857
[yann2.0] <[developpeur97427@gmail.com]>
2022-11-15 09:42:19 +0400

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+
+import './assets/main.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
+authentication()
+function authentication(){
+    axios.get("http://localhost:8080/bikeStoreBis/api/utilisateur")
+        .then(response=>{console.log(response.data)})
+        .catch(error=>{console.log(error)})
+}
+const app = createApp(App)
+
+app.use(router, axios)
+
+app.mount('#app')
